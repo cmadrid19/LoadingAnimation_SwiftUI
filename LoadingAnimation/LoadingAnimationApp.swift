a9245d0a211ec38be8144a41470cb6978cf5fb13
Max <maxim.macari.dam@gmail.com>
2020-12-29 12:25:59 +0100

    Initial Commit

---
+++ LoadingAnimation/LoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingAnimationApp.swift
+//  LoadingAnimation
+//
+//  Created by Maxim Macari on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
